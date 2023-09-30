6d9e2632b602358c50e0cfec2c62d0364da97216
Miguel <85888237+imkhoat@users.noreply.github.com>
2023-09-30 14:39:05 +0700

    feat: implement u-list-item components (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/storybook']
-})
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+        propsDestructure: true,
+      },
+    },
+  },
+  modules: ["@nuxt/ui", "@nuxtjs/storybook"],
+});
---
+++ types/components/u-list.d.ts
@@ -0,0 +1,6 @@
+import { UListItem } from "@/types/components/u-list-item";
+
+export interface UList extends UListItem{
+    items?: UListItem[],
+    modelValue?: string | object
+}
