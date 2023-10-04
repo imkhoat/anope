7208f983f0ee755c13b4076e9eb43388b8bbd116
Miguel <85888237+imkhoat@users.noreply.github.com>
2023-10-04 11:26:49 +0700

    fix: replace static key by i18n and update logic keep step value (#6)

--- pages/onboarding/_partials/step-enter-business-information.vue
+++ pages/onboarding/_partials/step-enter-business-information.vue
@@ -1,96 +1,93 @@
 <template>
     <div class="flex flex-col justify-center items-stretch gap-16 w-full h-full">
         <div class="flex flex-col justify-center items-stretch text-center gap-2">
-            <h6 class="text-3xl font-semibold">Enter your business information</h6>
-            <p class="text-sm opacity-60">The information we collect about your business to to facilitate automated
-                lodgerment</p>
+            <h6 class="text-3xl font-semibold">{{ $t(`onboarding.business.My Business Information`) }}</h6>
+            <p class="text-sm opacity-60"> {{ $t(`onboarding.business.The information we collect about your business`) }}</p>
         </div>
-        <u-card class="flex-grow" :ui="{
-            ring: '',
-            shadow: ''
-        }">
+        <u-card class="flex-grow" :ui="uiCard">
             <u-form :state="form" class="flex flex-col gap-y-6">
-                <u-form-group label="ABN / NZBN">
+                <u-form-group :label="$t(`general.form.ABN NZBN`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-input size="md" v-model="form.abn" class="col-span-8" placeholder="CS09202..."></u-input>
+                        <u-input size="md" v-model="form.businessABN" class="col-span-8" placeholder="CS09202..."></u-input>
                     </div>
                 </u-form-group>
-                <u-form-group label="Company name">
+                <u-form-group :label="$t(`general.form.Company name`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-input size="md" v-model="form.companyName" class="col-span-8" placeholder="Ardex Inc"></u-input>
+                        <u-input size="md" v-model="form.businessCompanyName" class="col-span-8" placeholder="Ardex Inc"></u-input>
                     </div>
                 </u-form-group>
-                <u-form-group label="Register business Address">
+                <u-form-group :label="$t(`general.form.Register business Address`)">
                     <div class="grid grid-cols-12 grid-rows-2 gap-2">
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-12"
-                            placeholder="Street address"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4" placeholder="Tower/City"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4" placeholder="State/Provide"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4"
-                            placeholder="ZIP/Postal code"></u-input>
+                        <u-input size="md" v-model="form.businessStreet" class="col-span-12"
+                            :placeholder="$t(`general.form.Street address`)"></u-input>
+                        <u-input size="md" v-model="form.businessCity" class="col-span-4" :placeholder="$t(`general.form.Tower City`)"></u-input>
+                        <u-input size="md" v-model="form.businessState" class="col-span-4" :placeholder="$t(`general.form.State Provide`)"></u-input>
+                        <u-input size="md" v-model="form.businessPostal" class="col-span-4"
+                            :placeholder="$t(`general.form.ZIP Postal code`)"></u-input>
                     </div>
                 </u-form-group>
 
-                <u-form-group label="Postal Address">
+                <u-form-group :label="$t(`general.form.Postal Address`)">
                     <div class="grid grid-cols-12 grid-rows-2 gap-2">
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-12"
-                            placeholder="Street address"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4" placeholder="Tower/City"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4" placeholder="State/Provide"></u-input>
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-4"
-                            placeholder="ZIP/Postal code"></u-input>
+                        <u-input size="md" v-model="form.businessPostalStreet" class="col-span-12"
+                            :placeholder="$t(`general.form.Street address`)"></u-input>
+                        <u-input size="md" v-model="form.businessPostalCity" class="col-span-4" :placeholder="$t(`general.form.Tower City`)"></u-input>
+                        <u-input size="md" v-model="form.businessPostalState" class="col-span-4" :placeholder="$t(`general.form.State Provide`)"></u-input>
+                        <u-input size="md" v-model="form.businessPostalPostal" class="col-span-4"
+                            :placeholder="$t(`general.form.ZIP Postal code`)"></u-input>
                     </div>
                 </u-form-group>
-                <u-form-group label="Email">
+                <u-form-group :label="$t(`general.form.Email`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-input size="md" v-model="form.jobTitle" class="col-span-8"
+                        <u-input size="md" v-model="form.businessEmail" class="col-span-8"
                             placeholder="your_email@email.com"></u-input>
                         <div class="col-span-4 flex flex-row justify-start items-start">
-                            <u-button size="md" variant="soft" icon="i-heroicons-plus-20-solid">Add more email</u-button>
+                            <u-button size="md" variant="soft" icon="i-heroicons-plus-20-solid">{{ $t(`general.button.Add more email`) }}</u-button>
                         </div>
                     </div>
                 </u-form-group>
-                <u-form-group label="Phone">
+                <u-form-group :label="$t(`general.form.Phone`)">
                     <div class="grid grid-cols-12 gap-2">
-                        <u-input size="md" v-model="form.phone" class="col-span-8" placeholder="+1 000-000-000"></u-input>
+                        <u-input size="md" v-model="form.businessPhone" class="col-span-8" placeholder="+1 000-000-000"></u-input>
                         <div class="col-span-4 flex flex-row justify-start items-start">
-                            <u-button size="md" variant="soft" icon="i-heroicons-plus-20-solid">Add more phone</u-button>
+                            <u-button size="md" variant="soft" icon="i-heroicons-plus-20-solid">{{ $t(`general.button.Add more phone`) }}</u-button>
                         </div>
-                        <u-input size="md" v-model="form.phone" class="col-span-8" placeholder="+1 000-000-000"></u-input>
+                        <u-input size="md" v-model="form.businessPhone2" class="col-span-8" placeholder="+1 000-000-000"></u-input>
                     </div>
                 </u-form-group>
-                <u-form-group label="Company website">
+                <u-form-group :label="$t(`general.form.Company website`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-input size="md" v-model="form.email2" class="col-span-8"
+                        <u-input size="md" v-model="form.businessCompanyWebsite" class="col-span-8"
                             placeholder="Senior house kepper"></u-input>
                     </div>
                 </u-form-group>
-                <u-form-group label="About">
+                <u-form-group :label="$t(`general.form.About`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-textarea v-model="form.about" class="col-span-12" placeholder="..."></u-textarea>
+                        <u-textarea v-model="form.businessAbout" class="col-span-12" placeholder="..."></u-textarea>
                     </div>
                 </u-form-group>
-                <u-form-group label="Country">
+                <u-form-group :label="$t(`general.form.Country`)">
                     <div class="grid grid-cols-12 gap-x-2">
                         <u-select-menu size="md" v-model="form.businessCountry" :options="countries" class="col-span-8"
                             placeholder="Australia"></u-select-menu>
                     </div>
                 </u-form-group>
-                <u-form-group label="Currency">
+                <u-form-group :label="$t(`general.form.Currency`)">
                     <div class="grid grid-cols-12 gap-x-2">
                         <u-select-menu size="md" v-model="form.businessCurrency" :options="currencies" class="col-span-8"
                             placeholder="AUD"></u-select-menu>
                     </div>
                 </u-form-group>
-                <u-form-group label="Datetime format" help="This present format base on datetime format">
+                <u-form-group :label="$t(`general.form.Datetime format`)" :help="$t(`general.form.This present format base on datetime format`)">
                     <div class="grid grid-cols-12 gap-x-2">
-                        <u-input size="md" v-model="form.country" class="col-span-8" placeholder="Australia"></u-input>
+                        <u-input size="md" v-model="form.businessDatetimeFormat" class="col-span-8" placeholder="Australia"></u-input>
                     </div>
                 </u-form-group>
             </u-form>
             <template #footer>
                 <div class="flex flex-row justify-end items-center gap-x-2">
-                    <u-button size="md" color="gray" variant="soft" class="px-6">Back</u-button> <u-button size="md">{{ $t('general.Save and Next Step') }}</u-button>
+                    <u-button size="md" color="gray" variant="soft" class="px-6">{{ $t(`general.button.Back`) }}</u-button>
+                    <u-button size="md">{{ $t(`general.button.Save and Next Step`) }}</u-button>
                 </div>
             </template>
         </u-card>
@@ -108,4 +105,20 @@ const { form } = inject<{
 // data
 const countries = ['United State', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
 const currencies = ['AUD', 'USD']
+
+
+const uiCard = {
+    ring: '',
+    shadow: '',
+    body: {
+        "base": "",
+        "background": "",
+        "padding": "px-8 py-10 sm:p-16"
+    },
+    footer: {
+        "base": "",
+        "background": "",
+        "padding": "px-8 py-5 sm:px-16 sm:py-6"
+    },
+}
 </script>
--- pages/onboarding/index.vue
+++ pages/onboarding/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="onboarding-page w-full h-full grid grid-cols-12 gap-2">
         <div class="left-side col-span-9 px-16 pt-16">
-            <step-update-profile v-if="activeStep === 'STEP_01'"></step-update-profile>
-            <step-enter-business-information v-else-if="activeStep === 'STEP_02'"></step-enter-business-information>
+            <step-update-profile v-if="activeStep === 'STEP-01'"></step-update-profile>
+            <step-enter-business-information v-else-if="activeStep === 'STEP-02'"></step-enter-business-information>
         </div>
         <div class="right-side min-h-full bg-white col-span-3 -my-4 -mr-4 flex flex-col justify-between items-stretch">
             <onboarding-stepper />
@@ -20,7 +20,7 @@ import StepUpdateProfile from './_partials/step-update-profile.vue';
 import StepEnterBusinessInformation from './_partials/step-enter-business-information.vue';
 
 // type
-
+type StepValue = 'STEP-01' | 'STEP-02' | 'STEP-03' | 'STEP-04' | 'STEP-05' | 'STEP-06' | 'STEP-07' | 'STEP-08' | 'STEP-09' | 'STEP-10'
 interface Step extends UStepItem {
     title: string,
     description: string,
@@ -32,54 +32,106 @@ definePageMeta({
     layout: "onboarding",
 });
 
+//composable
+const { t } = useI18n()
+const route = useRoute()
+
 // data
 const form = reactive({
-    email: undefined,
-    fullName: {
-        firstName: undefined,
-        lastName: undefined
-    },
-    password: undefined,
-    about: undefined,
-    jobTitle: undefined,
-    phone: undefined,
-    email2: undefined,
-    country: undefined,
+    profileEmail: undefined,
+    profileFirstName: undefined,
+    profileLastName: undefined,
+    profilePassword: undefined,
+    profileAbout: undefined,
+    profileJobTitle: undefined,
+    profileEmail2: undefined,
+    profileCountry: undefined,
+    profileStress: undefined,
+    profileCity: undefined,
+    profileState: undefined,
+    profilePostal: undefined,
+    profilePhone: undefined,
+    profilePhone2: undefined,
+    businessABN: undefined,
+    businessCompanyName: undefined,
+    businessStreet: undefined,
+    businessCity: undefined,
+    businessState: undefined,
+    businessPostal: undefined,
+    businessPostalStreet: undefined,
+    businessPostalCity: undefined,
+    businessPostalState: undefined,
+    businessPostalPostal: undefined,
+    businessEmail: undefined,
+    businessPhone: undefined,
+    businessPhone2: undefined,
+    businessCompanyWebsite: undefined,
+    businessAbout: undefined,
+    businessDatetimeFormat: undefined,
     businessCountry: 'United State',
     businessCurrency: 'AUD'
 })
 
 const steps = reactive<Partial<Step>[]>([
     {
-        title: 'Update your profile',
-        description: 'Fill you profile information to access potential data',
-        to: '#step-01',
-        value: 'STEP_01'
+        title: t('onboarding.steps.Update your profile'),
+        description: t(`onboarding.profile.The information we collect as part of your profile`),
+        to: '?step=step-01',
+        value: 'STEP-01'
     }, {
-        title: 'Enter your business information',
-        description: 'Fill you profile information to access potential data',
-        to: '#step-02',
-        value: 'STEP_02'
+        title: t('onboarding.steps.Enter your business information'),
+        description: t(`onboarding.business.The information we collect about your business`),
+        to: '?step=step-02',
+        value: 'STEP-02'
     }, {
-        title: 'Enter your property information',
+        title: t('onboarding.steps.Enter your property information'),
         description: 'Fill you profile information to access potential data',
-        to: '#step-01',
-        value: 'STEP_03'
+        to: '?step=step-03',
+        value: 'STEP-03'
     }, {
-        title: 'Setup your hourses',
+        title: t('onboarding.steps.Setup your hourses'),
         description: 'Fill you profile information to access potential data',
-        to: '#step-01',
-        value: 'STEP_04'
+        to: '?step=step-04',
+        value: 'STEP-04'
     }, {
-        title: 'Intergrate your Account System',
+        title: t('onboarding.steps.Intergrate your Account System'),
         description: 'Fill you profile information to access potential data',
-        to: '#step-01',
-        value: 'STEP_05'
+        to: '?step=step-05',
+        value: 'STEP-05'
+    }, {
+        title: t('onboarding.steps.Setup your Product and Service'),
+        description: 'Fill you profile information to access potential data',
+        to: '?step=step-06',
+        value: 'STEP-06'
+    }, {
+        title: t('onboarding.steps.Setup your Supplier'),
+        description: 'Fill you profile information to access potential data',
+        to: '?step=step-07',
+        value: 'STEP-07'
+    }, {
+        title: t('onboarding.steps.Setup your Contacts Customers'),
+        description: 'Fill you profile information to access potential data',
+        to: '?step=step-08',
+        value: 'STEP-08'
+    }, {
+        title: t('onboarding.steps.Invite Users'),
+        description: 'Fill you profile information to access potential data',
+        to: '?step=step-09',
+        value: 'STEP-09'
+    }, {
+        title: t('onboarding.steps.Completed'),
+        description: 'Fill you profile information to access potential data',
+        to: '?step=step-10',
+        value: 'STEP-10'
     }
 ])
-const activeStep = ref<'STEP_01' | 'STEP_02' | 'STEP_03'>('STEP_01')
+const activeStep = ref<StepValue>('STEP-01')
 
 
+//watch
+watchEffect(() => {
+    activeStep.value = route.query?.step?.toString().toUpperCase() as StepValue || 'STEP-01'
+})
 // propvide
 provide(onboardingInjectionKey, { form, steps, activeStep })
 </script>
