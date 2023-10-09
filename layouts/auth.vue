25a4a77eb78ad62d68ddb4e060ce290995a50afd
imkhoat <khoatbui.idea@gmail.com>
2023-10-09 17:00:21 +0700

    feat: add ui for login page

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,18 @@
 <template>
-  <div class="auth-layout"></div>
+  <div class="auth-layout flex flex-col justify-start items-stretch h-screen w-screen bg-gray-50">
+    <the-auth-header></the-auth-header>
+    <u-container class="flex justify-center items-start flex-grow">
+      <div class="w-full sm:w-96 mt-16">
+        <slot />
+      </div>
+    </u-container>
+  </div>
 </template>
+<script lang="ts" setup>
+import TheAuthHeader from '@/layouts/_partials/the-auth-header.vue'
+</script>
+<style>
+.auth-layout {
+  font-family: 'AR One Sans', sans-serif;
+}
+</style>
---
+++ pages/auth/_partials/form-login.vue
@@ -0,0 +1,31 @@
+<template>
+    <u-card>
+        <u-form class="flex flex-col justify-start items-stretch gap-y-8">
+            <div class="flex flex-col gap-y-1">
+                <u-avatar size="lg" class="bg-primary-100 border-8 border-primary-50">
+                    <u-icon size="xl" name="i-heroicons-shield-check-20-solid" class="text-primary-500"></u-icon>
+                </u-avatar>
+                <h6 class="font-semibold text-xl leading-0">New member!</h6>
+                <span class="text-sm text-gray-500 leading-0">Welcome to anope, manage your horses easyer</span>
+            </div>
+            <div class="flex flex-col justify-start items-stretch gap-y-2">
+                <u-form-group :label="$t('general.form.Email')">
+                    <u-input size="lg"></u-input>
+                </u-form-group>
+                <u-form-group :label="$t('general.form.Password')">
+                    <u-input size="lg"></u-input>
+                </u-form-group>
+                <u-form-group :label="$t('general.form.Confirm password')">
+                    <u-input size="lg"></u-input>
+                </u-form-group>
+            </div>
+            <div class="flex flex-col justify-start items-stretch gap-y-2">
+                <u-button size="lg" block>{{ $t(`general.button.Login`)}}</u-button>
+                <div class="flex flex-row justify-between items-stretch gap-x-2 text-center">
+                    <u-button size="lg" variant="soft" class="flex-grow text-center justify-center">Google</u-button>
+                    <u-button size="lg" variant="soft" class="flex-grow text-center justify-center">Twitter</u-button>
+                </div>
+            </div>
+        </u-form>
+    </u-card>
+</template>
