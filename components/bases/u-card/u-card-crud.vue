7d2ba57478e3bce43d0adc4dda2b73a7e3ffa5de
Miguel <85888237+imkhoat@users.noreply.github.com>
2023-10-19 16:10:03 +0700

    feat: add datepicker (#21)

---
+++ components/bases/u-card/u-card-crud.vue
@@ -0,0 +1,3 @@
+<template>
+    <u-card></u-card>
+</template>
--- pages/workspace/[id]/settings/users-management.vue
+++ pages/workspace/[id]/settings/users-management.vue
@@ -1,11 +1,14 @@
 <template>
-    <u-page page-title="Users Management" page-description="You can invite or remove workspace user">
+    <u-page page-title="Users Management" page-description="Invite or remove workspace user">
         <template #header-action>
-            <u-button size="lg" icon="i-heroicons-user-plus">Invite new users</u-button>
+            <div class="flex flex-row justify-end items-center gap-1">
+                <u-button color="gray" variant="soft" size="lg" icon="i-heroicons-arrow-path"></u-button>
+                <u-button size="lg" icon="i-heroicons-user-plus">Invite new users</u-button>
+            </div>
         </template>
         <div class="grid grid-cols-12 gap-4 h-fit">
             <div
-                class="col-span-12 md:col-span-10 lg:col-span-8 row-span-1 grid grid-cols-12 gap-4">
+                class="col-span-12 md:col-span-12 lg:col-span-10 row-span-1 grid grid-cols-12 gap-4">
                 <div class="col-span-12 sm:col-span-6 lg:col-span-3 row-span-1">
                     <summary-card v-bind="summaryData.totalUsers"></summary-card>
                 </div>
