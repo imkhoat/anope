ef4e4e339917177a11be409693b6a51e4b5cdf3f
Miguel <85888237+imkhoat@users.noreply.github.com>
2023-11-29 14:08:30 +0700

    feat: upgrade nuxt version (#42)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,38 +2,46 @@ import { removePagesMatching } from './utils/routingGenerate'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {
-        enable: true
-    },
-    hooks: {
-        'pages:extend'(pages) {
-            removePagesMatching(/_partials/, pages)
-            removePagesMatching(/_composables/, pages)
-        }
-    },
-    colorMode: {
-        preference: 'light'
-    },
-    vite: {
-        vue: {
-            script: {
-                defineModel: true,
-                propsDestructure: true
-            }
-        }
-    },
-    modules: ['@pinia/nuxt', '@nuxt/ui', '@nuxtjs/i18n', '@nuxtjs/storybook', '@nuxt/image', '@pinia/nuxt', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@nuxt/image'],
-    i18n: {
-        locales: [
-            { code: 'vi', name: 'Tiếng Viêt', iso: 'vi-VN', file: 'vi.json' },
-            { code: 'en', name: 'English', iso: 'en-EN', file: 'en.json' }
-        ],
-        defaultLocale: 'vi',
-        langDir: './locales'
-    },
-    googleFonts: {
-        families: {
-            'AR+One+Sans': true
-        }
+  runtimeConfig: {
+    oauth: {
+      auth0: {
+        clientId: '',
+        clientSecret: ''
+      }
     }
+  },
+  devtools: {
+    enable: true
+  },
+  hooks: {
+    'pages:extend'(pages) {
+      removePagesMatching(/_partials/, pages)
+      removePagesMatching(/_composables/, pages)
+    }
+  },
+  colorMode: {
+    preference: 'light'
+  },
+  vite: {
+    vue: {
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }
+  },
+  modules: ['nuxt-auth-utils', '@pinia/nuxt', '@nuxt/ui', '@nuxtjs/i18n', '@nuxtjs/storybook', '@nuxt/image', '@pinia/nuxt', '@nuxtjs/google-fonts', '@vueuse/nuxt', '@nuxt/image'],
+  i18n: {
+    locales: [
+      { code: 'vi', name: 'Tiếng Viêt', iso: 'vi-VN', file: 'vi.json' },
+      { code: 'en', name: 'English', iso: 'en-EN', file: 'en.json' }
+    ],
+    defaultLocale: 'vi',
+    langDir: './locales'
+  },
+  googleFonts: {
+    families: {
+      'AR+One+Sans': true
+    }
+  }
 })
