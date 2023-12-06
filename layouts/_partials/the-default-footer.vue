9ad190f90d51818c7196926b8be932a6550173da
imkhoat <khoatbui.idea@gmail.com>
2023-12-06 21:27:08 +0700

    style: move ui config to global app config

--- layouts/_partials/the-default-footer.vue
+++ layouts/_partials/the-default-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <u-container :ui="containerUI" class="flex justify-center items-center text-sm bg-gray-50">
-    <span class="opacity-50"> Made by anope team with</span> <u-icon name="i-heroicons-heart-20-solid" class="h-4 w-4 text-rose-600 ml-1" />
+    <span class="opacity-50"> Made by anope team with</span> <u-icon name="i-heroicons-heart-20-solid" class="h-4 w-4 text-error-600 ml-1" />
   </u-container>
 </template>
 
--- pages/workspace/_partials/onboarding/step-invite-user.vue
+++ pages/workspace/_partials/onboarding/step-invite-user.vue
@@ -35,7 +35,7 @@
               <u-button
                 size="xs"
                 variant="ghost"
-                color="rose"
+                color="error"
                 icon="i-heroicons-trash"
                 @click="onRemoveUser(row)"
               />
