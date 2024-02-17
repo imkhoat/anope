6941fb4e8059603f42ac8b5705b1e7a96c310081
imkhoat <khoatbui.idea@gmail.com>
2024-02-17 15:30:20 +0700

    fix: replace u-list with vertical navigation

--- pages/workspace/[id]/settings/index.vue
+++ pages/workspace/[id]/settings/index.vue
@@ -1,7 +1,11 @@
 <template>
   <u-page page-title="Setting" page-description="Choose between categories">
     <div class="grid grid-cols-12 gap-4 h-fit">
-      <div v-for="(menu, index) in menus" :key="index + '__setting-pages'" class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+      <div
+        v-for="(menu, index) in menus"
+        :key="index + '__setting-pages'"
+        class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3"
+      >
         <setting-menu :title="menu.title" :items="menu.items" />
       </div>
     </div>
@@ -15,87 +19,119 @@ import SettingMenu from '@/pages/workspace/_partials/settings/setting-menu.vue'
 const menus = ref([
   {
     title: 'Business Information',
-    items: [{
-      title: 'My Business Overview',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }]
+    items: [
+      {
+        label: 'My Business Overview',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      }
+    ]
   },
   {
     title: 'User management',
-    items: [{
-      title: 'Team and Groups',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'User Management',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'User Types and Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Role and Permissions',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }]
+    items: [
+      {
+        label: 'Team and Groups',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'User Management',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'User Types and Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Role and Permissions',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      }
+    ]
   },
   {
     title: 'Financial',
-    items: [{
-      title: 'Payment Settings',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }]
-  }, {
+    items: [
+      {
+        label: 'Payment Settings',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      }
+    ]
+  },
+  {
     title: 'Alert and Notification',
-    items: [{
-      title: 'My Business Overview',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }]
+    items: [
+      {
+        label: 'My Business Overview',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      }
+    ]
   },
   {
     title: 'Default and Reference data',
-    items: [{
-      title: 'Local setting',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Horses Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Contact Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Location Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Users Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }, {
-      title: 'Activity, Treatment and Proceduce Reference Data',
-      class: '!font-normal',
-      icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
-      value: 'WORKSPACE_01'
-    }]
+    items: [
+      {
+        label: 'Local setting',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Horses Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Contact Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Location Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Users Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      },
+      {
+        label: 'Activity, Treatment and Proceduce Reference Data',
+        class: '!font-normal',
+        icon: 'i-heroicons-arrow-top-right-on-square-20-solid',
+        value: 'WORKSPACE_01',
+        to: '/workspace'
+      }
+    ]
   }
 ])
 </script>
--- pages/workspace/_partials/settings/setting-menu.vue
+++ pages/workspace/_partials/settings/setting-menu.vue
@@ -3,36 +3,34 @@
     <template #header>
       <u-card-header :title="title" :description="description" />
     </template>
-    <u-list :items="items">
-      <template #item="{ item }">
-        <u-list-item v-bind="item" class="overflow-hidden">
-          <template #icon="{ icon }">
-            <u-avatar v-if="icon" size="xs" class="ring-2 ring-gray-50">
-              <u-icon class="w-6 h-6" :name="icon" />
-            </u-avatar>
-          </template>
-        </u-list-item>
-      </template>
-    </u-list>
+    <u-vertical-navigation :links="items" :ui="verticalUI" />
   </u-card>
 </template>
 
 <script lang="ts" setup>
-import UList from '@/components/bases/u-list/u-list.vue'
-import UListItem from '@/components/bases/u-list/u-list-item.vue'
 import UCardHeader from '@/components/bases/u-card/u-card-header.vue'
+import type { VerticalNavigationLink } from '@nuxt/ui/dist/runtime/types'
 
 defineProps<
   Partial<{
     title: string;
     description: string;
-    items: {
-      title: string;
-      description?: string;
-      icon: string;
-      value: string;
-      class?: string;
-    }[];
+    items: VerticalNavigationLink[][];
   }>
 >()
+
+const verticalUI = {
+  padding: 'px-2.5 py-2.5',
+  font: 'font-normal',
+  active:
+    'text-gray-900 dark:text-white before:bg-gray-100 dark:before:bg-gray-800',
+  inactive:
+    'text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50',
+  icon: {
+    base: 'flex-shrink-0 w-5 h-5 font-bold ml-2 relative',
+    active: 'text-gray-700 dark:text-gray-200',
+    inactive:
+      'text-gray-600 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-800'
+  }
+}
 </script>
